c85544dd892b00fcc6f54e3f1237b81d43f05c55
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-11-07 12:56:58 -0700

    style about section

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 // particlesJS.load('particles-js', 'assets/particles.json', function() {
 //   console.log('callback - particles.js config loaded');
 // });
+
+$(".btn").click(function() {
+  $('html,body').animate({
+      scrollTop: $(".section-about").offset().top},
+      'slow');
+});
